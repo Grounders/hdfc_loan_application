76427dca51090b067b963a59d8ddc7230cff72e1
Grounders <110928357+Grounders@users.noreply.github.com>
2026-02-02 22:20:38 +0530

    Update App.java
    a new code update is done.

--- src/main/java/com/evom/App.java
+++ src/main/java/com/evom/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "welcome to simplilearn CEHv13" );
+         System.out.println( "You will learn a lot here" );
     }
 }
