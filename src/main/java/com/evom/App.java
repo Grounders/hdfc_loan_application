bf3bf80dfce7566e55f4aa0259af7f0105bdb612
sanjeev <sanjeev.itprofessioal@gmail.com>
2026-02-03 23:01:22 +0530

    conflict resolve

--- src/main/java/com/evom/App.java
+++ src/main/java/com/evom/App.java
@@ -10,10 +10,10 @@ public class App
     {
         System.out.println( "welcome to simplilearn CEHv13" );
          System.out.println( "You will learn a lot here" );
-<<<<<<< HEAD
+
 	System.out.println( "3rd line of code" );
-=======
+
            System.out.println( "Y3rd line of code is added" );
->>>>>>> ff32d2440b8eba8123d1c0a28936e0bc194ea45a
+
     }
 }
